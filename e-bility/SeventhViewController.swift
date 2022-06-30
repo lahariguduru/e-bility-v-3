f22285ea4e420011f0af6dbfd92f6d85996d192a
Lahari Guduru <lguduru1214@gmail.com>
2022-06-30 14:19:51 -0500

    version at 2:19 pm thurs

---
+++ e-bility/SeventhViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SeventhViewController.swift
+//  e-bility
+//
+//  Created by Scholar on 6/30/22.
+//
+
+import UIKit
+
+class SeventhViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
